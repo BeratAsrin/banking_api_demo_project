51842e8335ecb0f6abb8396bbc4b95e80371bb87
Berat Asrın CAFEROĞLU <beratasrin01@gmail.com>
2022-11-17 20:45:11 +0300

    Exception is deleted.

--- src/main/java/com/banking_demo_project/banking_api/constants/exceptions/CouldNotCreateException.java
+++
@@ -1,8 +0,0 @@
-package com.banking_demo_project.banking_api.constants.exceptions;
-
-public class CouldNotCreateException extends Exception{
-    public CouldNotCreateException(){}
-    public CouldNotCreateException(String message){
-        super(message);
-    }
-}
